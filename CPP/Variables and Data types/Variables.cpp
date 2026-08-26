05e7064428a4be9e4ce3a46985e72c8c8def9056
mohammadaashik-ux <mohammadaashik276@gmail.com>
2026-08-26 11:23:44 +0530

    auto update

--- CPP/Variables and Data types/Variables.cpp
+++ CPP/Variables and Data types/Variables.cpp
@@ -1,17 +1,21 @@
-/*
+
 #include <iostream>
+#include <string>                // we use string on our code always in cloud the string header file
 using namespace std;
 
+/*
 int main() {
     // Variable declaration
-    int age = 25; // Integer variable
-    double height = 5.9; // Double variable
-    char grade = 'A'; // Character variable
-    bool isStudent = true; // Boolean variable
-    const string name = "AASHIK"; // Constant variable
-
+    */
+    int age = 25;                   // Integer variable         // Declaration and Initialization 
+    double height = 5.9;            // Double variable
+    char grade = 'A';               // Character variable
+    bool isStudent = true;          // Boolean variable
+    const string name = "AASHIK";   // Constant variable
+    float gpa = 3.72; 
+/*
     // Output the values of the variables
-    cout << "Age: " << age << endl;  // endl is used to insert a new line after the output 
+    cout << "Age: " << age << endl;         // endl is used to insert a new line after the output 
     cout << "Height: " << height << endl;
     cout << "Grade: " << grade << endl;
     cout << "Is Student: " << isStudent << endl;
@@ -21,16 +25,26 @@ int main() {
 } 
 */
 
-#include <iostream>
-using namespace std;
 
 int main() {
 
-    int age;
+    /*
+    int age;                    // Declaration
     cout << "Enter your age: ";
-    cin >> age;         // cin is used to take input from the user and >> - extraction operator
+    cin >> age;                 // cin is used to take input from the user and >> - extraction operator
     cout << "Your age is: " << age << endl;
-}
+
+    char letter = 95;       // ascii format also work
+
+    cout << "Letter: " << letter << endl;
+
+    cout << "Size of Age " <<sizeof(age) << " Bytes"<< endl;
+    cout << "Size of Height " <<sizeof(height) << " Bytes"<< endl;
+    cout << "Size of GPA " <<sizeof(gpa) << " Bytes"<< endl;
+    cout << "Size of Bool " <<sizeof(isStudent) << " Bytes"<< endl;
+    cout << "Size of Grade " <<sizeof(grade) << " Bytes"<< endl;
+*/
+
 
 /*
 Rules for creating an identifier in C++:
@@ -51,4 +65,32 @@ Data types in C++:
 - char - Character data type
 - bool - Boolean data type
 - void - Void data type
+- string - string data type
 */
+
+
+/*
+Logical Operators
+
+&& - Logical AND
+|| - Logical OR
+! - Logical NOT 
+
+*/
+
+//          ------- STRING -------
+
+string first_name = "Mohammad";
+string last_name = "Aashik";
+
+cout<< first_name+" "+last_name << endl; // string concatination
+cout<< first_name.append(last_name) << endl;
+
+cout<< first_name.length() << endl; // 14
+cout<< first_name.size() << endl;
+cout<< first_name[10] << endl; //
+cout<< first_name[13] << endl;
+
+return 0;
+
+}
