7ed43f7554dab7fc7740d92745f42e6f6c3e0ac2
mohammadaashik-ux <mohammadaashik276@gmail.com>
2026-08-28 21:39:52 +0530

    auto update

--- CPP/02. Conditional_Statement/if_else.cpp
+++ CPP/02. Conditional_Statement/if_else.cpp
@@ -2,19 +2,29 @@
 using namespace std;
 
 int main(){
+
     int age = 0;
     cout<< "Enter Your Age: ";
     cin>> age;
 
-    if(age >= 18)
-    {
-        cout<< "Your are a Adult"<<endl;
-    }
+    // if(age < 18)
+    // {
+    //     cout<< "Your are a Child"<<endl;
+    // }
 
-    else
-    {
-        cout<< "Your are a not Adult"<<endl;
-    }
+    // else if(age > 65){
+    //     cout<< "Your are a Senior Citizen"<<endl;
+    // }
+    // else
+    // {
+    //     cout<< "Your are a Adult"<<endl;
+    // }
+
+    //              ---------------------- OR ----------------------
+
+    (age>=18) ? cout<< "Your are a Adult" : cout<< "Your are a Child";
 
     return 0;
 }
+
+//              ---------------------- OR ----------------------
---
+++ CPP/02. Conditional_Statement/switch_case.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int day = 0;
+    cout<< "Enter Your Day Number(1-7): ";
+    cin >> day;
+
+    if (day<=0 || day>7){
+        cout<< "Please enter your number brtween 1-7"<<endl;
+        cout<<endl;
+        cout<< "Enter Your Day Number(1-7): ";
+        cin >> day;
+    }
+
+    switch (day)
+    {
+    case 1:
+        /* code */
+        cout<< "The Number of Day is Sunday"<<endl;
+        break;
+
+    case 2:
+        /* code */
+        cout<< "The Number of Day is Monday"<<endl;
+        break;
+    
+    case 3:
+        /* code */
+        cout<< "The Number of Day is Tuesday"<<endl;
+        break;
+
+    case 4:
+        /* code */
+        cout<< "The Number of Day is Wednesday"<<endl;
+        break;
+
+    case 5:
+        /* code */
+        cout<< "The Number of Day is Thursday"<<endl;
+        break;
+
+    case 6:
+        /* code */
+        cout<< "The Number of Day is Friday"<<endl;
+        break;
+
+    case 7:
+        /* code */
+        cout<< "The Number of Day is Saturday"<<endl;
+        break; 
+           
+    default:
+        break;
+    }
+
+    return 0;
+}    
